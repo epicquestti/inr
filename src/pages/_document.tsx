0410c5bff5e8863b54a8a1e77cfc49462fcc343c
Douglas Pacor <douglas@epicquestti.com.br>
2022-05-05 16:24:17 -0300

    fix api last publishes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,40 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="description" content="Inr Publicações" />
           <meta charSet="utf-8" />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-touch-icon-76x76.png"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/apple-touch-icon-120x120.png"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/apple-touch-icon-152x152.png"
+          />
+
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
