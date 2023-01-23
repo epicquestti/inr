83cbf4038dc05db8e76087f66f1122b5006d6201
Douglas Pacor <douglas@epicquestti.com.br>
2023-01-23 10:26:08 -0300

    doing

--- src/lib/backend/database/index.ts
+++ src/lib/backend/database/index.ts
@@ -6,15 +6,18 @@ const papr = new Papr({})
 
 export async function connect() {
   if (paprConfig.uri) {
+    console.log("LOG DB:", process.env.NODE_ENV)
+
     if (process.env.NODE_ENV === "production") {
       client = await MongoClient.connect(paprConfig.uri)
     } else {
       if (!global.mongo) {
         global.mongo = await MongoClient.connect(paprConfig.uri)
       }
+
+      client = global.mongo
     }
 
-    client = global.mongo
     papr.initialize(client.db(paprConfig.bdName))
     await papr.updateSchemas()
   }
